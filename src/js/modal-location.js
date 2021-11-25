20d471721090b66c05876a3299072fab0993818b
Yana-Yeg <yanayeg@gmail.com>
2021-11-25 20:19:34 +0200

    add js

---
+++ src/js/modal-location.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('.our-location-open'),
+        closeModalBtn: document.querySelector('.our-location-close'),
+        modal: document.querySelector('.our-location'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+})();
