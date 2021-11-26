e16f9f92671c547023c180a20a0055105331f06f
KorsunVV <korsunvv.work@gmail.com>
2021-11-26 21:41:49 +0200

    nomodal

--- src/js/modal-location.js
+++ src/js/modal-location.js
@@ -1,8 +1,23 @@
 (() => {
     const refs = {
-        openModalBtn: document.querySelector('.our-location-opener'),
+        openModalBtn: document.querySelector('.contacts-section__btnlocation'),
         closeModalBtn: document.querySelector('.our-location-close'),
         modal: document.querySelector('.our-location'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+})();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('.contacts-section__btnfranchize'),
+        closeModalBtn: document.querySelector('.our-franchize-close'),
+        modal: document.querySelector('.our-franchize'),
     };
 
     refs.openModalBtn.addEventListener('click', toggleModal);
