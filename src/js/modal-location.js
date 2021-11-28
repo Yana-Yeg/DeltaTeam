f393501a9f521d241cefefcf2ae325315bf30842
Yana-Yeg <yanayeg@gmail.com>
2021-11-28 19:28:05 +0200

    add header-modal

--- src/js/modal-location.js
+++ src/js/modal-location.js
@@ -33,21 +33,39 @@
 
 
 // _____________________________product
-$(document).ready(function(){
-	$('.products-list-item__btn1').click(function(){
-		$('.content_block1').slideToggle(300);      
-		return false;
-	});
+$(document).ready(function () {
+    $('.products-list-item__btn1').click(function () {
+        $('.content_block1').slideToggle(300);
+        return false;
+    });
 });
-$(document).ready(function(){
-	$('.products-list-item__btn2').click(function(){
-		$('.content_block2').slideToggle(300);      
-		return false;
-	});
+$(document).ready(function () {
+    $('.products-list-item__btn2').click(function () {
+        $('.content_block2').slideToggle(300);
+        return false;
+    });
 });
-$(document).ready(function(){
-	$('.products-list-item__btn3').click(function(){
-		$('.content_block3').slideToggle(300);      
-		return false;
-	});
+$(document).ready(function () {
+    $('.products-list-item__btn3').click(function () {
+        $('.content_block3').slideToggle(300);
+        return false;
+    });
 });
+
+// __MODAL BUY NOW__
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('.buy-modal-open'),
+        closeModalBtn: document.querySelector('.buy-modal-close'),
+        modal: document.querySelector('.buy-now-modal'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+})();
